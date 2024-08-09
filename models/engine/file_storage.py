bd3247a590db1280946900c9e152e7a4743d3d2e
Oyematth01 <oyematth@gmail.com>
2024-08-09 22:15:13 +0100

    task

---
+++ models/engine/file_storage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module defines a class to manage file storage for the HBNB clone."""
+import datetime
+import json
+import os
+
+
+class FileStorage:
+
+    """Class for storing and retrieving data"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
